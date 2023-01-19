62e6c4182d7217af598d9215c328582ba60f2439
Alexander Soudry <soudryalex@gmail.com>
2023-01-19 18:58:31 -0500

    added cors

--- HehlApi/Program.cs
+++ HehlApi/Program.cs
@@ -7,6 +7,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(p => p.AddPolicy(
+    "cors", build => {
+        build.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod();
+    }
+));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +21,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("cors");
+
 
 app.UseHttpsRedirection();
 
